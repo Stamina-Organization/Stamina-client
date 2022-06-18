e409b847a0f850b88262cb86c28ff8356382bc97
Gipson62 <J.H.Gipson62@gmail.com>
2022-06-18 16:12:43 +0200

    Système de détection de connexion
    Début d'un système qui affiche si l'on est connecté ou non via un statut en bas à gauche. Et d'un bouton se déverrouillant lorsqu'on ne l'est plus pour pouvoir se reconnecter

--- launcher/launcher.gd
+++ launcher/launcher.gd
@@ -2,6 +2,8 @@ extends Control
 
 # The URL we will connect to
 @export var SOCKET_URL = "ws://localhost:4545"
+@onready var connexion_true = preload("res://assets/icons/connexion_true.png")
+@onready var connexion_false = preload("res://assets/icons/connexion_false.png")
 
 @onready var settings_windows = $Background/Settings
 @onready var credits_windows = $Background/Credits
@@ -12,20 +14,7 @@ extends Control
 var _client = WebSocketClient.new()
 
 func _ready():
-	# Connect base signals to get notified of connection open, close, and errors.
-	_client.connection_closed.connect(_on_connection_closed)
-	_client.connection_error.connect(_on_connection_closed)
-	_client.connection_established.connect(_on_connected)
-	# This signal is emitted when not using the Multiplayer API every time
-	# a full packet is received.
-	# Alternatively, you could check get_peer(1).get_available_packets() in a loop.
-	_client.data_received.connect(_on_data)
-	
-	# Initiate connection to the given URL.
-	var err = _client.connect_to_url(SOCKET_URL, ["lws-mirror-protocol"])
-	if err != OK:
-		print("Unable to connect")
-		set_process(false)
+	connexion()
 
 func _on_SettingsButton_pressed():
 	if show_settings == false:
@@ -54,9 +43,14 @@ func _on_credits_button_pressed():
 func _on_connection_closed(was_clean = false):
 	print("Closed, clean : ", was_clean)
 	set_process(false)
+	$Background/content/ConnexionState.icon = connexion_false
+	$Background/content/ConnexionState.disabled = false
+	$Background/UnableToConnect.show()
 
 func _on_connected(proto = ""):
 	print("Connected with protocole : ", proto)
+	$Background/content/ConnexionState.icon = connexion_true
+	$Background/content/ConnexionState.disabled = true
 
 func _on_data():
 	# Print the received packet, you MUST always use get_peer(1).get_packet
@@ -70,3 +64,24 @@ func _send():
 	# and not put_packet directly when not using the MultiplayerAPI.
 	var test:String = "Test Packet"
 	_client.get_peer(1).put_packet(test.to_utf8_buffer())
+
+func connexion():
+	# Connect base signals to get notified of connection open, close, and errors.
+	_client.connection_closed.connect(_on_connection_closed)
+	_client.connection_error.connect(_on_connection_closed)
+	_client.connection_established.connect(_on_connected)
+	# This signal is emitted when not using the Multiplayer API every time
+	# a full packet is received.
+	# Alternatively, you could check get_peer(1).get_available_packets() in a loop.
+	_client.data_received.connect(_on_data)
+	
+	# Initiate connection to the given URL.
+	var err = _client.connect_to_url(SOCKET_URL, ["lws-mirror-protocol"])
+	if err != OK:
+		print("Unable to connect")
+		set_process(false)
+		$Background/UnableToConnect.show()
+
+
+func _on_connexion_state_pressed():
+	connexion()
